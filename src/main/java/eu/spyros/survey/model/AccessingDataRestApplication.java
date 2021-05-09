d8fe05ec5b3a5a6a15683dc32c689156d2c52fd3
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2021-05-09 17:00:23 +0300

    Added some data

--- src/main/java/eu/spyros/survey/model/AccessingDataRestApplication.java
+++ src/main/java/eu/spyros/survey/model/AccessingDataRestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class AccessingDataRestApplication {
     private static final Logger log = LoggerFactory.getLogger(AccessingDataRestApplication.class);
@@ -16,12 +18,25 @@ public class AccessingDataRestApplication {
         SpringApplication.run(AccessingDataRestApplication.class, args);
     }
 
+
     @Bean
-    public CommandLineRunner demo(
-            final CustomerRepository customerRepository,
-            final RideRepository rideRepository,
-            final RideSurveyRepository rideSurveyRepository,
-            final CustomerRideFeedbackRequestRepository rideFeedbackRequestRepository) {
+    public CommandLineRunner demo(CustomerRepository customerRepository,
+
+                                  RideRepository rideRepository,
+
+                                  RideSurveyRepository rideSurveyRepository,
+                                  RideSurveyResponseRepository rideSurveyResponseRepository,
+
+                                  CustomerRideFeedbackRequestRepository customerRideFeedbackRequestRepository,
+                                  CustomerRideFeedbackResponseRepository customerRideFeedbackResponseRepository,
+
+                                  FreeTextQuestionRepository freeTextQuestionRepository,
+                                  FreeTextAnswerRepository freeTextAnswerRepository,
+
+                                  ChoiceQuestionRepository choiceQuestionRepository,
+                                  ChoiceAnswerRepository choiceAnswerRepository,
+                                  ChoiceQuestionOptionRepository choiceQuestionOptionRepository
+    ) {
         return (args) -> {
             // save a few customers
             final Customer jackBauer = customerRepository.save(new Customer("Jack", "Bauer"));
@@ -30,13 +45,35 @@ public class AccessingDataRestApplication {
             final Customer davidPalmer = customerRepository.save(new Customer("David", "Palmer"));
             final Customer micheleDessler = customerRepository.save(new Customer("Michelle", "Dessler"));
 
+
+            //Create a survey
             final RideSurvey satisfactionSurvey = rideSurveyRepository.save(new RideSurvey());
+
+            final FreeTextQuestion freeTextQuestion = freeTextQuestionRepository.save(new FreeTextQuestion("What did you enjoy in the ride?", satisfactionSurvey));
+
+            final ChoiceQuestion multipleChoiceQuestion = choiceQuestionRepository.save(ChoiceQuestion.createMultipleChoice("What did you appreciate?", 3, satisfactionSurvey, List.of("Vehicle", "Music", "Safety")));
+            multipleChoiceQuestion.getChoiceQuestionOptions().stream().forEach(choiceQuestionOptionRepository::save);
+
+            final ChoiceQuestion singleChoiceQuestion = choiceQuestionRepository.save(ChoiceQuestion.createSingleChoice("Would you ride again?", satisfactionSurvey, List.of("Yes", "No")));
+            singleChoiceQuestion.getChoiceQuestionOptions().stream().forEach(choiceQuestionOptionRepository::save);
+
 
             final Ride jacksRide = rideRepository.save(new Ride(jackBauer));
 
-            final CustomerRideFeedbackRequest rideFeedbackRequest = rideFeedbackRequestRepository.save(new CustomerRideFeedbackRequest(jacksRide, satisfactionSurvey));
+            //Lets assume that the system requests for a survey
+            final CustomerRideFeedbackRequest customerRideFeedbackRequest = customerRideFeedbackRequestRepository.save(new CustomerRideFeedbackRequest(jacksRide, satisfactionSurvey));
 
-            final FreeTextQuestion freeTextQuestion = new FreeTextQuestion();
+            //first part of the CustomerRideFeedbackResponse
+            final CustomerRideFeedbackResponse customerRideFeedbackResponse = customerRideFeedbackResponseRepository.save(new CustomerRideFeedbackResponse(5.0, customerRideFeedbackRequest));
+
+            //First part of the RideSurveyResponse
+            final RideSurveyResponse rideSurveyResponse = rideSurveyResponseRepository.save(new RideSurveyResponse(customerRideFeedbackResponse));
+
+            //And the answers
+            final FreeTextAnswer freeTextAnswer = freeTextAnswerRepository.save(new FreeTextAnswer("The lack of traffic", "", rideSurveyResponse, freeTextQuestion));
+            final ChoiceAnswer multipleChoiceAnswer = choiceAnswerRepository.save(ChoiceAnswer.createMultipleChoiceAnswer("", multipleChoiceQuestion, rideSurveyResponse, multipleChoiceQuestion.getChoiceQuestionOptions()));
+
+            final ChoiceAnswer singleChoiceAnswer = choiceAnswerRepository.save(ChoiceAnswer.createSingleChoiceAnswer("", multipleChoiceQuestion, rideSurveyResponse, singleChoiceQuestion.getChoiceQuestionOptions().stream().findFirst().get()));
 
 
         };
