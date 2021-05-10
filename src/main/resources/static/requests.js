078d38433b4eb4146d3d6893cbba5b5fa010dffa
Spyros <21125056+SpyrosKou@users.noreply.github.com>
2021-05-10 04:02:30 +0300

    To be potentially used in get requests

---
+++ src/main/resources/static/requests.js
@@ -0,0 +1,28 @@
+/**
+ * Simple async get request
+ * @param theUrl
+ * @param callback
+ */
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+}
+
+/**
+ * Simple sync get request
+ * @param theUrl
+ * @returns {string}
+ */
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.response;
+}
